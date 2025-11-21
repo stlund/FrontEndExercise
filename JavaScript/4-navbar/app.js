e535c383765108f08087b13baa82c678ffdec70e
Thomas Östlund <thomas.ostlund@gmail.com>
2025-11-21 11:22:07 +0100

    added exercise2 - Toggle button for changing text and element style.

--- JavaScript/4-navbar/app.js
+++ JavaScript/4-navbar/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+//give me the object of .nav-toggle and .links
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+//listen for click and toggle the manu |||
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
